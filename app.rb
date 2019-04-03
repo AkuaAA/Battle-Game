c16cebe5e7e9b626d1cb322c7ff15f41c1035eeb
Akua Apeagyei <apeagyei.akua@gmail.com>
2019-04-03 15:01:37 +0100

    added game class

---
+++ app.rb
@@ -0,0 +1,29 @@
+require 'sinatra/base'
+require './lib/player.rb'
+
+class Battle < Sinatra::Base
+  enable :sessions
+
+get '/' do
+  erb :index
+end
+
+post '/names' do
+  $player_1 = Player.new(params[:player_1_name])
+  $player_2 = Player.new(params[:player_2_name])
+   redirect '/play'
+ end
+
+ get '/play' do
+   @player_1 = $player_1
+   @player_2 = $player_2
+   erb :play
+ end
+
+ get '/attack' do
+  @player_1 = $player_1
+  @player_2 = $player_2
+  Game.new.attack(@player_2)
+  erb :attack
+  end
+end
